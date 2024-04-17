3a2de7a327f8459efaa77a9ed6adb67f6b7ae175
satyanikithdandamudi <dsnd123456@gmail.com>
2024-04-17 17:04:16 +0530

    small-change

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,7 +2,7 @@ const Home = () => {
   return (
     <div className="home-container">
       <div>
-        Home
+        Stress managment
         <img src="https://d37v7cqg82mgxu.cloudfront.net/img/home/sun.svg" alt='sun'/>
       </div>
     </div>
